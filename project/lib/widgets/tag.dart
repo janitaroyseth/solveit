ace209dd50d0acb58d82d5698fb8e37795e00377
janital <janital@stud.ntnu.no>
2022-09-14 18:26:14 +0200

    Fixed documentation of tag widget.

--- project/lib/widgets/tag.dart
+++ project/lib/widgets/tag.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 
+/// Options for sizing of tags.
 enum Size { small, large }
 
 /// Represents a tag which descibes a task.
 class Tag extends StatelessWidget {
-  /// The [size] of the tag og type .
+  /// The [size] of the tag, of type enum [Size].
   final Size size;
 
-  /// The [color] of the tag.
+  /// The [color] of the tag, of type [Color].
   final Color color;
 
-  /// [tagText] is the text in the tag.
+  /// [tagText] is the text in the tag, of type [String].
   final String tagText;
 
   /// Creates an instance of [Tag].
