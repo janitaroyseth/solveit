aa15699238cd967839bf6ae6aff8a20fc0edda69
janital <janital@stud.ntnu.no>
2022-10-12 12:54:34 +0200

    Started to add theming to the app.

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:project/screens/project_calendar_screen.dart';
 import 'package:project/screens/sign_in_screen.dart';
+import 'package:project/screens/task_detail_screen.dart';
+import 'package:project/screens/task_overview_screen.dart';
+import 'package:project/styles/theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,16 +16,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "solveIt",
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        fontFamily: "Comfortaa",
-      ),
+      theme: Themes.themeData,
       home: const SignInScreen(),
     );
   }
 }
-
-
-
-
-
