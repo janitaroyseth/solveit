8903afe0ed4b2c6e749e0a8345dc63781df6c185
norseninja <corezon@hotmail.com>
2022-10-20 23:36:06 +0200

    Fix: Changed taskListItem and tagsList to take Tag as parameter instead of TagWidget.

--- project/lib/widgets/tags_list.dart
+++ project/lib/widgets/tags_list.dart
@@ -5,7 +5,7 @@ import '../models/tag.dart';
 /// Represents a list containing [TagWidget].
 class TagsList extends StatelessWidget {
   /// The [tags] in the [TagList].
-  final List<dynamic> tags;
+  final List<Tag> tags;
   final Size size;
 
   /// Creates an instance of tag list, where [tags] is the list of tags represented in the list.
@@ -13,17 +13,13 @@ class TagsList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    if (size == Size.large) {
-      List<TagWidget> newTags = [];
-      for (Tag tag in tags) {
-        newTags.add(TagWidget(
-          tagText: tag.text,
-          size: Size.large,
-          color: Color(tag.color),
-        ));
-      }
-      tags.clear();
-      tags.addAll(newTags);
+    List<Widget> newTags = [];
+    for (Tag tag in tags) {
+      newTags.add(TagWidget(
+        tagText: tag.text,
+        size: size,
+        color: Color(tag.color),
+      ));
     }
     return Wrap(
       spacing: 1.0,
@@ -35,7 +31,7 @@ class TagsList extends StatelessWidget {
       alignment: WrapAlignment.start,
 
       /// How the children within a run should be placed in the main axis.
-      children: tags as List<Widget>,
+      children: newTags,
     );
   }
 }
