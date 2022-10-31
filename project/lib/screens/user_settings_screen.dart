42ffd2cdaac3de827728b37f69061d99e292a9cb
norseninja <corezon@hotmail.com>
2022-10-31 17:55:05 +0100

    Resolves #53 - Added user_settings_screen according to issue specifications.

---
+++ project/lib/screens/user_settings_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+/// Scaffold/Screen for viewing and editing user settings.
+class UserSettingsScreen extends StatelessWidget {
+  const UserSettingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
