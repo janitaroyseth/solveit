cc89f51457f69cfcd97a61c624abb76624e26f0b
Sakarias <post@sakarias.no>
2022-11-16 11:52:01 +0100

    Added sign in with Google

--- project/lib/services/auth_service.dart
+++ project/lib/services/auth_service.dart
@@ -1,5 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:google_sign_in/google_sign_in.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 abstract class AuthService {
   User? get currentUser;
@@ -8,7 +8,7 @@ abstract class AuthService {
   Future<User?> signInAnonymously();
 
   /// Signing in with Google.
-  // Future<User?> signInWithGoogle();
+  Future<User?> signInWithGoogle();
 
   /// Sign out.
   Future<void> signOut();
@@ -27,32 +27,32 @@ class Auth implements AuthService {
     return userCredentials.user;
   }
 
-  // @override
-  // Future<User?> signInWithGoogle() async {
-  //   final googleSignIn = GoogleSignIn();
-  //   final googleUser = await googleSignIn.signIn();
-  //   if (googleUser != null) {
-  //     final googleAuth = await googleUser.authentication;
-  //     if (googleAuth.idToken != null) {
-  //       final userCredential = await _fireBaseAuth
-  //           .signInWithCredential(GoogleAuthProvider.credential(
-  //         idToken: googleAuth.idToken,
-  //         accessToken: googleAuth.accessToken,
-  //       ));
-  //       return userCredential.user;
-  //     } else {
-  //       throw FirebaseAuthException(
-  //         code: "ERROR_MISSING_GOOGLE_ID_TOKEN",
-  //         message: "Missing Google ID token",
-  //       );
-  //     }
-  //   } else {
-  //     throw FirebaseAuthException(
-  //       code: "ERROR_ABORTED_BY_USER",
-  //       message: "Sign in aborted by user",
-  //     );
-  //   }
-  // }
+  @override
+  Future<User?> signInWithGoogle() async {
+    final googleSignIn = GoogleSignIn();
+    final googleUser = await googleSignIn.signIn();
+    if (googleUser != null) {
+      final googleAuth = await googleUser.authentication;
+      if (googleAuth.idToken != null) {
+        final userCredential = await _fireBaseAuth
+            .signInWithCredential(GoogleAuthProvider.credential(
+          idToken: googleAuth.idToken,
+          accessToken: googleAuth.accessToken,
+        ));
+        return userCredential.user;
+      } else {
+        throw FirebaseAuthException(
+          code: "ERROR_MISSING_GOOGLE_ID_TOKEN",
+          message: "Missing Google ID token",
+        );
+      }
+    } else {
+      throw FirebaseAuthException(
+        code: "ERROR_ABORTED_BY_USER",
+        message: "Sign in aborted by user",
+      );
+    }
+  }
 
   @override
   Future<void> signOut() async {
