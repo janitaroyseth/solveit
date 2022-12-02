19d9a54a276f636876ec74c10c1d828fe5afb455
janital <janital@stud.ntnu.no>
2022-12-02 14:32:10 +0100

    Created unit tests for groups

--- project/lib/models/group.dart
+++ project/lib/models/group.dart
@@ -1,11 +1,20 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+/// Represents a group of user's in a chat conversation.
 class Group {
+  /// The id of the group.
   String groupId;
+
+  /// List of the member's user id's.
   List<String> members;
+
+  /// The most recent message sent.
   String recentMessage;
+
+  /// When the group was last updated.
   DateTime lastUpdated;
 
+  /// Creates an instance of [Group].
   Group({
     this.groupId = "",
     required this.members,
@@ -13,6 +22,7 @@ class Group {
     DateTime? lastUpdated,
   }) : lastUpdated = lastUpdated ?? DateTime.now();
 
+  /// Returns this instance of groups as a [Map<String, dynamic].
   Map<String, dynamic> toMap() {
     return {
       "groupId": groupId,
@@ -22,13 +32,27 @@ class Group {
     };
   }
 
-  static Group? fromMap(Map<String, dynamic> data) {
+  /// Convers the given [data] of [Map<String, dynamic] to a new instance
+  /// of [Group]. If data wasn't valid [null] is returned.
+  static Group? fromMap(Map<String, dynamic>? data) {
+    if (data == null) {
+      return null;
+    }
+
     final groupId = data["groupId"];
+
+    if (data["members"] == null) return null;
     final members = data["members"].cast<String>();
-    final recentMessage = data["recentMessage"];
+
+    final recentMessage = data["recentMessage"] ?? "";
+
     final lastUpdated = data["lastUpdated"] != null
         ? (data["lastUpdated"] as Timestamp).toDate()
         : null;
+
+    if (groupId == null || members == null) {
+      return null;
+    }
 
     return Group(
       groupId: groupId,
@@ -38,14 +62,29 @@ class Group {
     );
   }
 
+  /// Creates new instances of [Group] for the list
+  /// given, returns the list of group.
   static List<Group> fromMaps(var data) {
     List<Group> groups = [];
     for (var value in data) {
-      Group? group = fromMap(value.data());
+      Group? group;
+      if (data is List<Map<String, dynamic>>) {
+        group = fromMap(value);
+      } else {
+        group = fromMap(value.data());
+      }
       if (group != null) {
         groups.add(group);
       }
     }
     return groups;
+  }
+
+  @override
+  int get hashCode => groupId.hashCode;
+
+  @override
+  bool operator ==(Object other) {
+    return groupId == (other as Group).groupId;
   }
 }
